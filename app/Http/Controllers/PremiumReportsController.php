6914cb8c24b11c19c614a2fec7e2392abfe0d316
Yoosin Paddy <yoosinfrancis@gmail.com>
2021-06-22 00:28:59 +0300

    premiumsms table SMS callback

---
+++ app/Http/Controllers/PremiumReportsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PremiumReports;
+use Illuminate\Http\Request;
+
+class PremiumReportsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\PremiumReports  $premiumReports
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PremiumReports $premiumReports)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\PremiumReports  $premiumReports
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PremiumReports $premiumReports)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\PremiumReports  $premiumReports
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PremiumReports $premiumReports)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\PremiumReports  $premiumReports
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PremiumReports $premiumReports)
+    {
+        //
+    }
+}
---
+++ database/migrations/2021_06_21_211513_create_premium_reports_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePremiumReportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('premium_reports', function (Blueprint $table) {
+            $table->id();
+            $table->string('id');
+            $table->string('status');//Sent: The message has successfully been sent by our network.
+                                            //Submitted: The message has successfully been submitted to the MSP (Mobile Service Provider).
+                                            //Buffered: The message has been queued by the MSP.
+                                            // Rejected: The message has been rejected by the MSP. This is a final status.
+                                            //Success: The message has successfully been delivered to the receiver’s handset. This is a final status.
+                                            //Failed: The message could not be delivered to the receiver’s handset. This is a final status.
+            $table->string('phoneNumber');
+            $table->string('networkCode');
+            $table->string('failureReason')->nullable();
+            $table->string('retryCount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('premium_reports');
+    }
+}
