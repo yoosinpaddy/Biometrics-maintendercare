cb75176a299b77bb51c4ff2083aef895be575386
Muturi KImangu <danmuturikimangu@gmail.com>
2021-09-12 21:46:41 +0300

    personalising login page per school

--- resources/views/school/login.blade.php
+++ resources/views/school/login.blade.php
@@ -9,7 +9,7 @@
 <body class="hold-transition login-page">
     <div class="login-box">
         <div class="login-logo">
-            <a href="{{route('dashboard')}}"><b>Tender </b>Care</a>
+            <a href="{{route('dashboard')}}">{{$_ENV['APP_NAME']}}</a>
         </div>
         <!-- /.login-logo -->
         <div class="card">
